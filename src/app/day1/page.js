4d928b749c1ba908982e1026aae17dee05ac7dc5
rooihond <dahak076@gmail.com>
2025-06-03 21:20:45 +0200

    Embed YouTube video in Day 1 page and update placeholder text

--- src/app/day1/page.js
+++ src/app/day1/page.js
@@ -66,7 +66,18 @@ export default function Day1() {
           </p>
         </div>
         <div className="mt-12 text-center">
-          <p className="text-green-700 italic">Photos and videos coming soon...</p>
+          <div className="flex justify-center">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/fJgnLe2qRA4"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              className="rounded-lg shadow-lg"
+            ></iframe>
+          </div>
         </div>
       </div>
     </main>
